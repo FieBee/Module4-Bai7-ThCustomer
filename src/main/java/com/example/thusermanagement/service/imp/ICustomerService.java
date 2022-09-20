5c3299f04fddde65431462dd98af1645abf4bbbd
FieBee <phuonglf98@gmail.com>
2022-09-20 17:09:10 +0700

    ok

---
+++ src/main/java/com/example/thusermanagement/service/imp/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.example.thusermanagement.service.imp;
+
+import com.example.thusermanagement.model.Customer;
+import com.example.thusermanagement.service.IGeneralService;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
---
+++ src/main/java/com/example/thusermanagement/service/imp/IProvinceService.java
@@ -0,0 +1,7 @@
+package com.example.thusermanagement.service.imp;
+
+import com.example.thusermanagement.model.Province;
+import com.example.thusermanagement.service.IGeneralService;
+
+public interface IProvinceService extends IGeneralService<Province> {
+}
